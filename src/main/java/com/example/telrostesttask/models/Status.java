6d72f0fac4c961a84bdcdb446ce68394ea6ae9e4
justbegginer <blackcloudsinthesky@protonmail.com>
2023-05-18 22:06:42 +0300

    add status class using for response of operation

---
+++ src/main/java/com/example/telrostesttask/models/Status.java
@@ -0,0 +1,15 @@
+package com.example.telrostesttask.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Status {
+    private boolean success;
+    private String message;
+}
